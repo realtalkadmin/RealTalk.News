421a0181e8435a2abb2e163658fd63ab6c0f4d3f
RealTalk Admin <admin@realtalk.news>
2018-06-18 18:27:52 -0700

    New cache, article, pictures

---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,3 @@
+<?php exit; ?>
+[2018-06-18 17:53:40] WARNING: Form 3079 > espd***************@gm***.com is already subscribed to the selected list(s)
+[2018-06-18 17:53:42] WARNING: Form 3079 > espd***************@gm***.com is already subscribed to the selected list(s)
